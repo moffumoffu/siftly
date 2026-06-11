ce82960a76fe6f3278e7bd85348988dc428eca05
moffumoffu <warrenleexy@gmail.com>
2026-06-11 14:30:35 +0800

    add health route

--- app.py
+++ app.py
@@ -66,5 +66,9 @@ def get_posts():
 def about():
     return render_template("about.html")
 
+@app.route("/health")
+def health():
+    return "ok", 200
+
 if __name__ == "__main__":
     app.run(debug=True)
